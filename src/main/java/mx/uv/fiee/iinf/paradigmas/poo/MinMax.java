695131192a300f24244b46d67f87bd140f47509e
Alexis 18 <dath_18@outlook.com>
2025-03-17 15:51:14 -0600

    Implementamos la interface IMinMax<T>, y ordenamos de menor a mayor usando Arrays.sort

--- src/main/java/mx/uv/fiee/iinf/paradigmas/poo/MinMax.java
+++ src/main/java/mx/uv/fiee/iinf/paradigmas/poo/MinMax.java
@@ -1,5 +1,22 @@
 package mx.uv.fiee.iinf.paradigmas.poo;
 
-public class MinMax
+import java.util.Arrays;
+
+public class MinMax<T> implements IMinMax<T>
 {
+    private final T[] lista;
+    public MinMax(T[] listado) {
+        this.lista = listado;
+        Arrays.sort(lista);
+    }
+    
+    @Override
+    public T min() {
+        return lista[0];
+    }
+
+    @Override
+    public T max() {
+        return lista[lista.length - 1];
+    }
 }
