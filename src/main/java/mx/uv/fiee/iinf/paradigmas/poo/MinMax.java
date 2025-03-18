946890c0a88d271c8e62f3abab38d771ea89d6ab
Alexis 18 <dath_18@outlook.com>
2025-03-17 20:03:46 -0600

    Convertimos en List<T> y lo ordenamos usando Collections.sort

--- src/main/java/mx/uv/fiee/iinf/paradigmas/poo/MinMax.java
+++ src/main/java/mx/uv/fiee/iinf/paradigmas/poo/MinMax.java
@@ -1,13 +1,16 @@
 package mx.uv.fiee.iinf.paradigmas.poo;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MinMax<T> implements IMinMax<T>
 {
     private final T[] lista;
     public MinMax(T[] listado) {
         this.lista = listado;
-        Arrays.sort(lista);
+        List<T> lista2 = Arrays.asList(lista);
+        Collections.sort((List) lista2);
     }
     
     @Override
